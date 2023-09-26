174772625b6ce9facf3ba9a2c35038bb53e87b9d
edilfonso2019 <benjaminedilfonso@gmail.com>
2023-09-26 10:44:23 -0500

    corregido el arranque

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const path = require("path");
 
-// Configuración para servir archivos estáticos desde la raíz
-app.use(express.static(__dirname));
+// Configura la ruta raíz para servir el archivo index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
+// Otras rutas y configuraciones de tu aplicación
+
+// Inicia el servidor en un puerto específico
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
